dff461f5a0cf4c7415ad600f7d968b1455ed77cb
Christos Gantidis <christosg8@gmail.com>
2018-07-19 23:21:20 +0300

    More thorough testing
    By using a randomly generated vector of numbers, you can more thoroughly
    test whether your sort functions work correctly.
    
    It helps also to compare your results with the results generated by the
    sort function of the standard library.

--- main.cpp
+++ main.cpp
@@ -3,39 +3,80 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <random>
 
 template<typename T>
-std::ostream &operator<<(std::ostream &stream, std::vector<T> &vec) {
+std::ostream &operator<<(std::ostream &stream, const std::vector<T> &vec) {
     for (const auto &item : vec) {
         stream << item << " ";
     }
+    stream << "\b\n";
     return stream;
 }
 
+template<typename T>
+bool operator==(const std::vector<T> &a, const std::vector<T> &b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+
+    auto it_a = a.begin();
+    auto it_b = b.begin();
+
+    while (it_a != a.end()) {
+        if (*it_a != *it_b) {
+            return false;
+        }
+
+        ++it_a;
+        ++it_b;
+    }
+
+    return true;
+}
+
+template<typename T>
+bool operator!=(const std::vector<T> &a, const std::vector<T> &b) {
+    return !(a == b);
+}
+
 int main() {
+    const int NUM_NUMS = 1000000;
+    const int MIN_VAL = -1000, MAX_VAL = 1000;
+
+    // fill a vector with NUM_NUMS random numbers in the range [MIN_VAL, MAX_VAL] that follow a uniform distribution
+    std::random_device dev;
+    std::mt19937 gen(dev());
+    std::uniform_int_distribution<int> dist(MIN_VAL, MAX_VAL);
+    std::vector<int> nums(NUM_NUMS);
+    std::generate(nums.begin(), nums.end(), [&dist, &gen]() {
+        return dist(gen);
+    });
+
+    std::vector<int> sorted(nums);
+    std::sort(sorted.begin(), sorted.end());
+
+    std::vector<int> nums_cpy;
+
     std::cout << "------------Counting Sort-------------" << '\n';
-    std::vector<int> arr_countS = {1, 4, 3, 5, 6, 2, 1, 2};
-    std::cout << "Unsorted: = " << arr_countS << '\n';
-    countingSort(arr_countS);
-    std::cout << "Sorted:   = " << arr_countS << "\n\n";
+    nums_cpy = nums;    // copy assignment
+    countingSort(nums_cpy);
+    if (nums_cpy != sorted) std::cout << "wrong sort!\n";
 
     std::cout << "------------Insertion Sort------------" << '\n';
-    std::vector<double> arr_insertionS = {1.0, 2.14, 4.3, 2.33, 5.67};
-    std::cout << "Unsorted: = " << arr_insertionS << '\n';
-    insertionSort(arr_insertionS);
-    std::cout << "Sorted:   = " << arr_insertionS << "\n\n";
+    nums_cpy = nums;    // copy assignment
+    insertionSort(nums_cpy);
+    if (nums_cpy != sorted) std::cout << "wrong sort!\n";
 
     std::cout << "------------QuickSort-----------------" << '\n';
-    std::vector<int> arr_quickS = {5, 6, 7, 2, -1, 2, 1, 2};
-    std::cout << "Unsorted: = " << arr_quickS << '\n';
-    quickSort(arr_quickS, 0, arr_quickS.size());
-    std::cout << "Sorted:   = " << arr_quickS << "\n\n";
+    nums_cpy = nums;    // copy assignment
+    quickSort(nums_cpy, 0, NUM_NUMS);
+    if (nums_cpy != sorted) std::cout << "wrong sort!\n";
 
     std::cout << "------------Merge Sort----------------" << '\n';
-    std::vector<int> arr_mergeS = {124, 26, 47, 12, -12, 42, 51, 12};
-    std::cout << "Unsorted: = " << arr_mergeS << '\n';
-    MergeSort(arr_mergeS, 0, arr_mergeS.size());
-    std::cout << "Sorted:   = " << arr_mergeS << "\n\n";
+    nums_cpy = nums;    // copy assignment
+    MergeSort(nums_cpy, 0, NUM_NUMS);
+    if (nums_cpy != sorted) std::cout << "wrong sort!\n";
 
     return 0;
 }
