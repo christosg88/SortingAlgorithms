7ffc9430dfd92008079fd6bd90ce0eb24755aceb
Christos Gantidis <christosg8@gmail.com>
2018-10-22 17:20:25 +0300

    Change method parameter list to accept iterators and implement parallel versions

--- main.cpp
+++ main.cpp
@@ -1,98 +1,51 @@
-#include "sorting.tpp"
+#include "CountingSort.hpp"
+#include "MergeSort.hpp"
+#include "QuickSort.hpp"
+
+#include "vector_helpers.hpp"
+#include "timeit.hpp"
 
 #include <iostream>
 #include <vector>
-#include <algorithm>
-#include <random>
-#include <chrono>
-
-template<typename T>
-std::ostream &operator<<(std::ostream &stream, const std::vector<T> &vec) {
-    for (const auto &item : vec) {
-        stream << item << " ";
-    }
-    stream << "\b\n";
-    return stream;
-}
-
-template<typename T>
-bool operator==(const std::vector<T> &a, const std::vector<T> &b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-
-    auto it_a = a.begin();
-    auto it_b = b.begin();
-
-    while (it_a != a.end()) {
-        if (*it_a != *it_b) {
-            return false;
-        }
-
-        ++it_a;
-        ++it_b;
-    }
-
-    return true;
-}
-
-template<typename T>
-bool operator!=(const std::vector<T> &a, const std::vector<T> &b) {
-    return !(a == b);
-}
+#include <cassert>
 
 int main() {
-    const int NUM_NUMS = 100000;
-    const int MIN_VAL = -1000, MAX_VAL = 1000;
+    auto nums = generate_random_vector<int>(10'000'000, -1'000, 1'000);
 
-    // fill a vector with NUM_NUMS random numbers in the range [MIN_VAL, MAX_VAL] that follow a uniform distribution
-    std::random_device dev;
-    std::mt19937 gen(dev());
-    std::uniform_int_distribution<int> dist(MIN_VAL, MAX_VAL);
-    std::vector<int> nums(NUM_NUMS);
-    std::generate(nums.begin(), nums.end(), [&dist, &gen]() {
-        return dist(gen);
-    });
-
-    std::vector<int> sorted(nums);
+    auto sorted(nums);
+    ti::ti cpp_sort_timer("std::sort");
     std::sort(sorted.begin(), sorted.end());
+    cpp_sort_timer.finish();
 
-    std::vector<int> nums_cpy;
+    auto nums_cpy(nums);
+    ti::ti counting_sort_timer("Counting Sort");
+    counting_sort<int>(std::begin(nums_cpy), std::end(nums_cpy));
+    counting_sort_timer.finish();
+    assert(nums_cpy == sorted);
 
-    std::chrono::time_point<std::chrono::high_resolution_clock> start;
-    std::chrono::time_point<std::chrono::high_resolution_clock> stop;
+    nums_cpy = nums;
+    ti::ti parallel_counting_sort_timer("Parallel Counting Sort");
+    parallel_counting_sort<int>(std::begin(nums_cpy), std::end(nums_cpy));
+    parallel_counting_sort_timer.finish();
+    assert(nums_cpy == sorted);
 
-    std::cout << "Counting Sort: ";
-    nums_cpy = nums;    // copy assignment
-    start = std::chrono::high_resolution_clock::now();
-    countingSort(nums_cpy);
-    stop = std::chrono::high_resolution_clock::now();
-    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count() << "ms\n";
-    if (nums_cpy != sorted) std::cout << "wrong sort!\n";
+    nums_cpy = nums;
+    ti::ti merge_sort_timer("Merge Sort");
+    merge_sort<int>(nums_cpy);
+    merge_sort_timer.finish();
+    assert(nums_cpy == sorted);
 
-    std::cout << "Insertion Sort: ";
-    nums_cpy = nums;    // copy assignment
-    start = std::chrono::high_resolution_clock::now();
-    insertionSort(nums_cpy);
-    stop = std::chrono::high_resolution_clock::now();
-    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count() << "ms\n";
-    if (nums_cpy != sorted) std::cout << "wrong sort!\n";
+    nums_cpy = nums;
+    ti::ti parallel_merge_sort_timer("Parallel Merge Sort");
+    parallel_merge_sort<int>(nums_cpy);
+    parallel_merge_sort_timer.finish();
+    assert(nums_cpy == sorted);
 
-    std::cout << "QuickSort: ";
-    nums_cpy = nums;    // copy assignment
-    start = std::chrono::high_resolution_clock::now();
-    quickSort(nums_cpy, 0, NUM_NUMS);
-    stop = std::chrono::high_resolution_clock::now();
-    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count() << "ms\n";
-    if (nums_cpy != sorted) std::cout << "wrong sort!\n";
-
-    std::cout << "Merge Sort: ";
-    nums_cpy = nums;    // copy assignment
-    start = std::chrono::high_resolution_clock::now();
-    MergeSort(nums_cpy, 0, NUM_NUMS);
-    stop = std::chrono::high_resolution_clock::now();
-    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count() << "ms\n";
-    if (nums_cpy != sorted) std::cout << "wrong sort!\n";
+    nums_cpy = nums;
+    ti::ti quick_sort_timer("Quick Sort");
+    quick_sort<int>(std::begin(nums_cpy), std::end(nums_cpy));
+    quick_sort_timer.finish();
+    assert(nums_cpy == sorted);
 
     return 0;
 }
